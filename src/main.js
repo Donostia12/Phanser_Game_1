64e49522de4b11a04b8413743d3b5a4eae9f6b18
Donostia12 <suncry7890@gmail.com>
2025-01-19 23:47:45 +0800

    new config

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 import "./style.css";
+
+import Phaser from "phaser";
+
+const sizes = {
+  height: 800,
+  width: 400,
+};
+const config = {
+  type: Phaser.WEBGL,
+  width: sizes.width,
+  height: sizes.height,
+  width: sizes.width,
+};
+
+const game = new Phaser.Game(config);
