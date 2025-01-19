1a6864ec99bebdb7739a562aed72dd058cca84ee
Donostia12 <suncry7890@gmail.com>
2025-01-19 23:55:52 +0800

    main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,32 @@
 import "./style.css";
 
-import Phaser from "phaser";
+import Phaser, { Physics } from "phaser";
 
 const sizes = {
   height: 800,
   width: 400,
 };
+const speedDown = 300;
+
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super("this");
+  }
+  preload() {}
+  create() {}
+  update() {}
+}
 const config = {
   type: Phaser.WEBGL,
   width: sizes.width,
   height: sizes.height,
   width: sizes.width,
+  Physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: speedDown },
+    },
+  },
 };
 
 const game = new Phaser.Game(config);
