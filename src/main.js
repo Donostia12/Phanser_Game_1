280fb6c43c912ac46961859ac93af11172996645
Donostia12 <suncry7890@gmail.com>
2025-01-20 09:55:20 +0800

    cukup setsapp

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-import Phaser, { Physics } from "phaser";
+import Phaser, { Physics, Scene } from "phaser";
 
 const sizes = {
   height: 800,
@@ -10,23 +10,28 @@ const speedDown = 300;
 
 class GameScene extends Phaser.Scene {
   constructor() {
-    super("this");
+    super("this Game ");
   }
-  preload() {}
-  create() {}
+  preload() {
+    this.load.image("logo", "/assets/Logo.ico");
+  }
+  create() {
+    this.add.image(0, 0, "logo");
+  }
   update() {}
 }
 const config = {
   type: Phaser.WEBGL,
   width: sizes.width,
   height: sizes.height,
-  width: sizes.width,
+  canvas: gamecanvas,
   Physics: {
     default: "arcade",
     arcade: {
       gravity: { y: speedDown },
     },
   },
+  Scene: { GameScene },
 };
 
 const game = new Phaser.Game(config);
