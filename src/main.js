c84f63f45fc43e0b8879c68edf22ad0be952d5e5
Donostia12 <suncry7890@gmail.com>
2025-01-27 11:20:08 +0800

    new logic

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const sizes = {
   width: 400,
 };
 const speedDown = 30;
-const respawnsarea= 390;
+const respawnsarea= 350;
 
 class GameScene extends Phaser.Scene {
   constructor() {
@@ -16,6 +16,7 @@ class GameScene extends Phaser.Scene {
     this.cursor
     this.playerspeed = 1000+ 50
     this.target 
+    this.floor
     this.point = 0
   }
   preload() {
@@ -25,20 +26,23 @@ class GameScene extends Phaser.Scene {
     this.load.image('apple','public/assets/apple.png')
   }
   create() {
-    // Tambahkan gambar latar belakang dan sesuaikan ukurannya dengan kanvas
-    this.add.image(0, 0, 'bg').setOrigin(0.0).setDisplaySize(sizes.width, sizes.height);
-    this.add.image(0, 0, 'tree').setOrigin(0.0);
-    this.player = this.physics.add.image(0, 0, 'basket').setOrigin(0.0).setScale(0.2);
+    
+    this.add.image(0, 0, 'bg').setOrigin(0.0).setDisplaySize(sizes.width, sizes.height)
+    this.add.image(0, 0, 'tree').setOrigin(0.0)
+    this.player = this.physics.add.image(0, 0, 'basket').setOrigin(0.0).setScale(0.2)
     this.player.x = 10
     this.player.y = 550
-    this.player.body.allowGravity = false;
+    this.player.body.allowGravity = false
+    this.player.body.setSize(250, 50)
     this.player.body.allowGravity  =false
+    this.player.setCollideWorldBounds(true)
     this.cursor = this.input.keyboard.createCursorKeys()
     
     this.target = this.physics.add
     .image(0, 0, 'apple')
     .setOrigin(0.0)
     .setScale(1.5);
+    this.target.body.setSize(25,25)
     this.physics.add.overlap(this.target,this.player,this.targetHit,null,this)
   }
   update() {
@@ -48,7 +52,7 @@ class GameScene extends Phaser.Scene {
         this.target.setX(this.getRandomx())
         this.target.setVelocityY(this.speedDown);
     }
-    console.log(this.point);
+
     const {left,right} = this.cursor
 
 
@@ -67,6 +71,7 @@ class GameScene extends Phaser.Scene {
     this.target.setY(0)
     this.target.setX(this.getRandomx())
     this.point++
+    document.getElementById('point').innerText = `Points: ${this.point}`; 
   }
 }
 const config = {
